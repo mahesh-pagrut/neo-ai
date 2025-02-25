042c37d3f7db82dcd331b44a14c6ef543f34b2f6
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-02-25 21:47:21 +0530

    added basic structure to web images assets and folder structure and neo functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import "./App.css"
+import va from "./assets/ai.png"
+import {CiMicrophoneOn} from "react-icons/ci"
+
 
 export default function App() {
   return (
-    <div>Home</div>
+    <div className='main'>
+      <img src={va} alt='neo main' id='sh'/>
+      <span>I'm Neo, your personal AI assistent model</span>
+      <button>Click here <CiMicrophoneOn/></button>
+    </div>
   )
 }
